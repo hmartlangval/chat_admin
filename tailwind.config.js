0fa543c542b03a60fc33423d78b597cf893feea9
nelvin-hmar <109198308+nelvin-tf@users.noreply.github.com>
2025-04-05 14:39:39 +0530

    settings with prompts dir, dynamicrepository generic, settings loader added, TailGrids UI library added

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,5 +23,23 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#FF9900",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+        },
+      },
+      "dark"
+    ],
+  },
 } 
